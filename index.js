d78e2c9513312dcfc05dc57734dbe412fb692145
simonmwaura <simonmwaura2090@gmail.com>
2024-04-18 13:35:40 +0300

    assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(someValue) {
+    //returns the number of blocks given a value
+    if (someValue>42){
+        return someValue-42
+    }
+     else if(42>someValue){
+        return 42-someValue
+        
+    }
+  }
+  function distanceFromHqInFeet(someValue) {
+  return distanceFromHqInBlocks(someValue)*264;
+    // call the distanceFromHqInBlocks function from inside the distanceFromHqInFeet function,
+    // passing the argument from distanceFromHqInFeet into distanceFromHqInBlocks
+  
+    // the return value of distanceFromHqInBlocks can then be used to calculate feet
+  }
+  function distanceTravelledInFeet(start, destination) {
+    //returns the number of feet traveled
+    if(destination>start){
+        return (destination-start)*264
+    }
+    if(start>destination){
+        return(start-destination)*264
+    }
+  }
+  function calculatesFarePrice (start, destination) {
+    const distance = distanceTravelledInFeet(start, destination);
+  
+    if (distance <= 400) {
+      return 0;
+    } else if (distance > 400 && distance <= 2000) {
+      return .02 * (distance - 400);
+    } else if (distance > 2000 && distance < 2500) {
+      return 25;
+    } else {
+      return 'cannot travel that far';
+    }
+  }
